dacdf1ec8585a1f28cc6199cdcffac2a47f92c8f
Tonny221 <anthonny14gabriell@gmail.com>
2022-09-20 16:23:19 -0400

    creating searchBar

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,28 @@
+import { Box } from '@mui/material'
+import React from 'react'
+import { useState } from 'react'
+import { useNavigate } from 'react-router'
+import { Paper, IconButton } from '@mui/material'
+import { SearchIcon } from '@mui/icons-material'
+
+const SearchBar = () => {
+    return (
+        <Paper
+            component='form'
+            onSubmit={() => {console.log('submited!')}}
+            sx={{
+                borderRadius: 20,
+                border: '1px solid #e3e3e3',
+                pl: 2,
+                boxShadow: 'none',
+                mr: { sm: 5 }
+            }}
+        >
+            <input
+                className='search-bar'
+            />
+        </Paper>
+    )
+}
+
+export default SearchBar
